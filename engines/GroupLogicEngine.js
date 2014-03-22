584c4ecf0c6e9e1d69e03ee72c6c05ff45392063
Kyle <sourcetree@yuniyasha.com>
2014-03-22 15:59:26 -0300

    Documented the GroupLogicEngine and changed some Variable/Method names for hopefully better readability.

--- engines/GroupLogicEngine.js
+++ engines/GroupLogicEngine.js
@@ -10,83 +10,174 @@
 
 zen.engines.GroupLogicEngine = function () {
 	this.logicCalls = {};
-	this.logicLoops = {};
+	this.intervals = {};
 	this.intervalID = null;
 	this.interval = 10;
 };
 
+/**
+ * Public addLogic 
+ *
+ * Adds a Logic Method to be called at a set interval
+ *
+ * @param id, String, the identifier for this Logic, to be used to add/remove/pause/resume it later
+ * @param logicMethod, Method, the logic method to be called
+ * @param interval, Number, the interval at which the logicMethod should be called
+ * @return void
+ */
 zen.engines.GroupLogicEngine.prototype.addLogic = function (id, logicMethod, interval) {
 	this.logicCalls[id] = {
 		'method': logicMethod,
 		'interval': interval
 	};
 
-	if (!this._hasLogicLoop(interval)) {
+	if (!this._hasInterval(interval)) {
 		//Create the Interval for this Interval of Logic Loop
-		this._createLogicLoop(interval);
+		this._createInterval(interval);
 	}
 
-	this._addToLogicLoop(id, interval);
+	this._addToInterval(id, interval);
 };
 
+/**
+ * pauseLogic 
+ *
+ * Pauses the Logic Method but keeps it in storage to be resumed later
+ *
+ * @param id, String, identifier for the logic
+ * @return void
+ */
 zen.engines.GroupLogicEngine.prototype.pauseLogic = function (id) {
-	this._removeFromLogicLoop(id);
+	this._removeFromInterval(id);
 };
 
+/**
+ * resumeLogic 
+ *
+ * Resumes a paused logic method to continue getting called at it's interval
+ *
+ * @param id, String, idenfitier for the logic
+ * @return void
+ */
 zen.engines.GroupLogicEngine.prototype.resumeLogic = function (id) {
-	this._addToLogicLoop(id);
+	this._addToInterval(id);
 };
 
+/**
+ * removeLogic 
+ *
+ * Removes a Logic completely.  Stops looping it and removes it from this classes memory
+ *
+ * @param id, String, identifier for the logic
+ * @return void
+ */
 zen.engines.GroupLogicEngine.prototype.removeLogic = function (id) {
-	this._removeFromLogicLoop(id);
+	this._removeFromInterval(id);
 	delete this.logicCalls[id];
 };
 
-zen.engines.GroupLogicEngine.prototype.start = function () {
-	var self = this;
-	this.intervalID = setInterval(function () {
-		self._logicLoop();
-	}, this.interval);
-};
+// /**
+//  * start 
+//  *
+//  * methoddescription
+//  *
+//  * @param none
+//  * @return void
+//  */
+// zen.engines.GroupLogicEngine.prototype.start = function () {
+// 	var self = this;
+// 	this.intervalID = setInterval(function () {
+// 		self._logicLoop();
+// 	}, this.interval);
+// };
 
-zen.engines.GroupLogicEngine.prototype.stop = function () {
-	clearInterval(this.intervalID);
-};
+// zen.engines.GroupLogicEngine.prototype.stop = function () {
+// 	clearInterval(this.intervalID);
+// };
 
-zen.engines.GroupLogicEngine.prototype._createLogicLoop = function (interval) {
+/**
+ * _createInterval 
+ *
+ * Creates a object which references the ID of an interval,
+ * and a collection of all the methods to be run in that interval
+ *
+ * @param interval, Number, the interval this Logic Loop gets called in
+ * @return void
+ */
+zen.engines.GroupLogicEngine.prototype._createInterval = function (interval) {
 	var self = this;
-	this.logicLoops[interval] = {
+	this.intervals[interval] = {
 		'methods': [],
 		'interval_id': 	setInterval(function () {
-							self._logicLoop(interval);
+							self._interval(interval);
 						}, interval)
 	};
 };
 
-zen.engines.GroupLogicEngine.prototype._hasLogicLoop = function (interval) {
-	return this.logicLoops[interval] != undefined;
+/**
+ * _hasInterval
+ *
+ * Checks to see if there is a interval active	 
+ *
+ * @param interval, Number, the interval speed to check
+ * @return Boolean
+ */
+zen.engines.GroupLogicEngine.prototype._hasInterval = function (interval) {
+	return this.intervals[interval] != undefined;
 };
 
-zen.engines.GroupLogicEngine.prototype._removeLogicLoop = function (interval) {
-	clearInterval(this.logicLoops[interval].interval_id);
-	delete this.logicLoops[interval];
+/**
+ * _removeInterval 
+ *
+ * Removes a Interval and all of it's methods from being ran
+ *
+ * @param interval, Number, the interval speed this interval represents
+ * @return void
+ */
+zen.engines.GroupLogicEngine.prototype._removeInterval = function (interval) {
+	clearInterval(this.intervals[interval].interval_id);
+	delete this.intervals[interval];
 };
 
-zen.engines.GroupLogicEngine.prototype._addToLogicLoop = function (id) {
-	this.logicLoops[this.logicCalls[id].interval].methods.push(this.logicCalls[id].method);
+/**
+ * _addToInterval 
+ *
+ * Adds Logic to a Interval
+ *
+ * @param id, String, Identifier for the logic
+ * @return void
+ */
+zen.engines.GroupLogicEngine.prototype._addToInterval = function (id) {
+	this.intervals[this.logicCalls[id].interval].methods.push(this.logicCalls[id].method);
 };
 
-zen.engines.GroupLogicEngine.prototype._removeFromLogicLoop = function (id) {
+/**
+ * _removeFromInterval 
+ *
+ * Removes a Logic from being ran in an Interval
+ *
+ * @param id, String, Identifier for the logic
+ * @return void
+ */
+zen.engines.GroupLogicEngine.prototype._removeFromInterval = function (id) {
 	var interval = this.logicCalls[id].interval;
-	this.logicLoops[interval].methods.splice(this.logicLoops[interval].methods.indexOf(this.logicCalls[id].method), 1);
+	this.intervals[interval].methods.splice(this.intervals[interval].methods.indexOf(this.logicCalls[id].method), 1);
 	
-	if (this.logicLoops[interval].methods.length === 0) {
-		this._removeLogicLoop(interval);
+	if (this.intervals[interval].methods.length === 0) {
+		this._removeInterval(interval);
 	}
 };
 
-zen.engines.GroupLogicEngine.prototype._logicLoop = function (interval) {
-	for (var i in this.logicLoops[interval].methods) {
-		this.logicLoops[interval].methods[i]();
+/**
+ * _interval 
+ *
+ * Gets called by each Interval to run all the Logic Methods in that Interval
+ *
+ * @param Interval, Number, The Interval calling this
+ * @return void
+ */
+zen.engines.GroupLogicEngine.prototype._interval = function (interval) {
+	for (var i in this.intervals[interval].methods) {
+		this.intervals[interval].methods[i]();
 	}
 };
