9f320e5dd3e864cec8fe713e5d0a14a127a31bb5
Norman <devnull@localhost>
2016-04-04 18:55:36 -0300

    Improved the KeyboardCOntroller class and their events.

--- inputs/KeyboardController.js
+++ inputs/KeyboardController.js
@@ -1,11 +1,19 @@
 
 zen.inputs.KeyboardController = function(inputManager) {
 	zen.inputs.Controller.call(this, inputManager);
+	this._keyCodes = [];
+	this._eventDetails = {
+		keyCodes : [],
+		altKey : false,
+		shiftKey : false,
+		superKey : false
+	};
 };
 
 zen.extends(zen.inputs.Controller, zen.inputs.KeyboardController, {
 	_keyDownHandler : null,
 	_keyUpHandler : null,
+	_keyCodes : null,
 
 	destroy : function() {
 		if (this._keyUpHandler) {
@@ -21,36 +29,60 @@ zen.extends(zen.inputs.Controller, zen.inputs.KeyboardController, {
 	_attachEvents : function() {
 		var self = this;
 
+		//TODO: make this better
+		var _fireEvent = function() {
+			if (self._eventDetails.keyCodes.length > 0) {
+				self._onKeyDown(self._eventDetails)
+			}
+			window.requestAnimationFrame(_fireEvent);
+		};
+
+		window.requestAnimationFrame(_fireEvent);
+
 		this._keyDownHandler = function(e) {
-			self._onKeyDown(e);
+			e.preventDefault();
+			if (self._eventDetails.keyCodes.indexOf(e.keyCode) === -1) {
+				self._updateEventDetail(e);
+				self._onKeyPress();
+			}
 		};
 
 		this._keyUpHandler = function(e) {
-			self._onKeyUp(e);
+			e.preventDefault();
+			self._updateEventDetail(e);
+			self._onKeyUp();
 		};
 
 		window.addEventListener('keydown', this._keyDownHandler);
 		window.addEventListener('keyup', this._keyUpHandler);
 	},
 
-
-	_onKeyDown : function(e) {
-		e.preventDefault();
-		this._fireEvent('keydown', this._getEventDetail(e));
+	_onKeyPress : function() {
+		this._fireEvent('keypress', this._eventDetails);
 	},
 
-	_onKeyUp : function(e) {
-		e.preventDefault();
-		this._fireEvent('keyup', this._getEventDetail(e));
+	_onKeyDown : function() {
+		this._fireEvent('keydown', this._eventDetails);
 	},
 
-	_getEventDetail : function(e) {
-		console.log(e);
-		return  {
-			keyCode : e.keyCode,
-			altKey : e.altKey,
-			shiftKey : e.shiftKey,
-			superKey : e.metaKey
-		};
+	_onKeyUp : function() {
+		this._fireEvent('keyup', this._eventDetails);
+	},
+
+	_updateEventDetail : function(e) {
+		this._eventDetails.altKey = e.altKey;
+		this._eventDetails.shiftKey = e.shiftKey;
+		this._eventDetails.superKey = e.metaKey;
+		this._eventDetails.keyCode = e.keyCode;
+		if (e.type === 'keydown') {
+			if (this._eventDetails.keyCodes.indexOf(e.keyCode) === -1) {
+				this._eventDetails.keyCodes.push(e.keyCode);
+			}
+		}
+		else if (e.type === 'keyup') {
+			if (this._eventDetails.keyCodes.indexOf(e.keyCode) > -1) {
+				this._eventDetails.keyCodes.splice(this._eventDetails.keyCodes.indexOf(e.keyCode), 1);
+			}
+		}
 	}
 });
