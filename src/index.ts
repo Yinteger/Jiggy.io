03d8115cf130b907a395cff27bf46143415e28c3
Norman Breau <norman@normanbreau.com>
2017-10-22 19:11:12 -0300

    removed typings file. Fixed dependencines in package. Added idnex.ts for general package exports.

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+export * from './Engine';
+export * from './assets';
+export * from './audio';
+export * from './engines';
+export * from './entities';
+export * from './inputs';
+export * from './interfaces';
+export * from './utils';
